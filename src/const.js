5dfc6c50fef001ca98cfd9b49b3f3d9a5a7d9e14
nazmul53p <nazmul2018s@gmail.com>
2024-10-13 23:29:20 +0600

    set dynamic

--- src/const.js
+++ src/const.js
@@ -1,3 +1,5 @@
+// for front-end
+
 export const getDockerFile = nodeVersion => {
   return `
 FROM ${nodeVersion}
@@ -118,4 +120,112 @@ pipelines:
             - bash ./deploy.sh
 
   `;
+};
+
+// for back-end
+
+export const getDockerFileForBackendNode = nodeVersion => {
+  return `
+# Use the official Node.js image as the base image
+FROM ${nodeVersion}
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the dependencies file to the working directory
+COPY package.json .
+
+# Install all the dependencies
+RUN npm install pm2 -g && npm install
+
+# Copy the content of the server folder to the working directory
+COPY . .
+
+# Expose the port the app runs in
+EXPOSE 3010
+
+# Command to run the server
+CMD ["npm", "run", "start:dev"]
+`;
+};
+
+export const getDockerComposeFileForBackendNode = projectName => {
+  return `
+version: '3.8'
+services:
+  app:
+    build:
+      context: ./
+      dockerfile: Dockerfile
+    image: ${projectName}
+    tty: true
+    restart: unless-stopped
+    container_name: ${projectName}
+    working_dir: /app/
+    volumes:
+      - ./:/app
+    networks:
+      - documentation
+    ports:
+      - '3010:3010'
+    depends_on:
+      - db
+      - redis
+
+  db:
+    image: mysql:8.0
+    container_name: ${projectName}_db
+    restart: unless-stopped
+    command: --max_allowed_packet=32505856
+    environment:
+      MYSQL_DATABASE: ${projectName}
+      MYSQL_ROOT_PASSWORD: 123456
+      SERVICE_TAGS: dev
+      SERVICE_NAME: mysql
+      TZ: Asia/Dhaka
+    volumes:
+      - mysql-data:/var/lib/mysql
+    networks:
+      - documentation
+    ports:
+      - '3306:3306'
+
+  phpmyadmin:
+    depends_on:
+      - db
+    image: phpmyadmin/phpmyadmin
+    restart: always
+    ports:
+      - '80:80'
+    environment:
+      PMA_VERBOSE: 'Docker MySQL,Local MySQL'
+      MYSQL_ROOT_PASSWORD: 123456
+    networks:
+      - documentation
+
+  redis:
+    image: redis:alpine
+    container_name: ${projectName}_redis
+    ports:
+      - '6379:6379'
+    networks:
+      - documentation
+
+networks:
+  documentation:
+    driver: bridge
+volumes:
+  mysql-data:
+`;
+};
+
+export const getDeployShFileForBackendNode = projectName => {
+  return `
+git pull
+docker-compose down
+docker-compose up -d
+docker exec ${projectName} npm ci
+docker exec ${projectName} npm run build
+docker exec ${projectName} pm2 start --only "${projectName}-prod"
+`;
 };
