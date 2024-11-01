3cb2f6e4efc9ea3bc846c12d597c6e39410e5fcc
nazmul53p <nazmul2018s@gmail.com>
2024-11-01 20:38:47 +0600

    fix:  Discount e value entry hole remarks/Description is required

--- src/index.js
+++ src/index.js
@@ -84,7 +84,7 @@ const questions = [
     choices: [
       'frontend-(react or next.js)',
       'node-(express or nest.js)',
-      'php-(laravel)',
+      'php-(coming soon)',
       'python-(coming soon)',
     ],
     default: 'frontend (react or next.js)',
